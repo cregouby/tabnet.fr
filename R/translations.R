cd85005d7b4e05fd329b1a64a3b4340f1de26a6e
C. Regouby <christophe.regouby@free.fr>
2024-11-12 14:13:06 +0100

    switch to latest {rhelpi18n}

--- R/translations.R
+++ R/translations.R
@@ -1,2 +1,15 @@
+rd_flat_read <- function(file) {
+  rd_flat <- yaml::read_yaml(file)
+  attr(rd_flat, "untranslatable") <- rd_flat[["untranslatable"]]
+  rd_flat[["untranslatable"]] <- NULL
+  rd_flat
+}
+
+
 #' @export
-translations <- rhelpi18n::read_translations()
+translations <- (function() {
+  files <- list.files("translations/", full.names = TRUE)
+  translations <- lapply(files, rd_flat_read)
+  names(translations) <- tools::file_path_sans_ext(basename(files))
+  translations
+})()
